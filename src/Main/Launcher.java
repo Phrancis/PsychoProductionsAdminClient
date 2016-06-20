8c1e78cdfcad8050af593fc224465657ef14636c
Phrancis <music@psychoproductions.net>
2016-06-20 15:03:26 -0400

    initial commit

---
+++ src/Main/Launcher.java
@@ -0,0 +1,11 @@
+package Main;
+
+/**
+ * Main class and entry point of the application.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        // TODO make the application launch from here
+        System.out.println("PsychoProductionsAdminClient Launcher");
+    }
+}
