dc65dfdfa455765cf48c23dcb04aca35d2d4ae6a
Phrancis <music@psychoproductions.net>
2016-06-20 17:47:07 -0400

    Update Launcher

--- src/Main/Launcher.java
+++ src/Main/Launcher.java
@@ -1,11 +1,21 @@
 package Main;
 
+import GUI.LaunchSplashScreen;
+
+import javax.swing.*;
+
+
 /**
  * Main class and entry point of the application.
  */
 public class Launcher {
     public static void main(String[] args) {
-        // TODO make the application launch from here
+
         System.out.println("PsychoProductionsAdminClient Launcher");
+
+        Runnable splash = LaunchSplashScreen::new;
+
+        SwingUtilities.invokeLater(splash);
+
     }
 }
