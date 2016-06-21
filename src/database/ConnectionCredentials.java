a53ae53677464b3f1f748c0359c38b5a9b6405fd
Phrancis <music@psychoproductions.net>
2016-06-21 17:36:17 -0400

    Add encryption to password in ConnectionCredentials

--- src/database/ConnectionCredentials.java
+++ src/database/ConnectionCredentials.java
@@ -1,18 +1,37 @@
 package database;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+
 /**
  * Hold and make available credentials to connect to SQL database.
  */
 public class ConnectionCredentials {
     private String serverName = "";
-    private String username = "";
-    private String password = "";
     private final String DATABASE_NAME = "Francis";
+    private String username = "";
+    private byte[] encryptedPassword;
 
     public ConnectionCredentials(String serverName, String username, String password) {
         this.serverName = serverName;
         this.username = username;
-        this.password = password;
+        encryptedPassword = encrypt(password);
+    }
+
+    private byte[] encrypt(String password) {
+        // Encryption code based on
+        // http://stackoverflow.com/a/32583766/3626537
+        byte[] encrypted = {};
+        try {
+            Key passwordKey = new SecretKeySpec(password.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, passwordKey);
+            encrypted = cipher.doFinal(password.getBytes());
+        } catch(Exception exc) {
+            exc.printStackTrace();
+        }
+        return encrypted;
     }
 
     @Override
@@ -48,7 +67,11 @@ public class ConnectionCredentials {
         this.username = username;
     }
 
+    private byte[] getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
     public void setPassword(String password) {
-        this.password = password;
+        encryptedPassword = encrypt(password);
     }
 }
