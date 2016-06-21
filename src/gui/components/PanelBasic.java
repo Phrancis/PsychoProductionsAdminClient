9946673a8ae143512c34f8c4746809220c2cc90c
Phrancis <music@psychoproductions.net>
2016-06-21 13:42:16 -0400

    Create PanelBasic component

---
+++ src/gui/components/PanelBasic.java
@@ -0,0 +1,15 @@
+package gui.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Default panel template
+ */
+public class PanelBasic extends JPanel {
+
+    public PanelBasic(int width, int height) {
+        super();
+        setPreferredSize(new Dimension(width, height));
+    }
+}
